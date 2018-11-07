9a1214007dccc29955cadc19239dc8d3e3eb7d18
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-11-06 14:16:27 -1000

    server is running on 8989

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //require('dotenv').config();
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const bodyParser = require('body-parser');
 //const productRouter = require('./routes/products.js')
-const PORT = process.env.EXPRESS_CONTAINER_PORT;
+const PORT = process.env.EXPRESS_HOST_PORT || 3000;
 
 app.get('/', (req, res)=>{
-  console.log('hello')
+  res.send('hello')
 })
 console.log('hello')
+
+app.listen(PORT, (req, res)=>{
+  console.log(`Server is listening on ${PORT}`);
+});
